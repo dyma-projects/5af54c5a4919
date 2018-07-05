a206fea21c0be4d356f90dcc84e7c64ed020f9e3
HUAULT Christophe <christophe.huault@socomec.com>
2018-07-05 16:05:18 +0200

    exercice4

---
+++ src/app/exercice4/exercice4.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-exercice4',
+  templateUrl: './exercice4.component.html',
+  styleUrls: ['./exercice4.component.css']
+})
+export class Exercice4Component implements OnInit {
+
+  public texts:  Array<string> = [];
+
+  constructor() {
+    this.texts.push("un");
+    this.texts.push("deux");
+    this.texts.push("trois");
+    
+
+   }
+
+  ngOnInit() {
+  }
+
+}
